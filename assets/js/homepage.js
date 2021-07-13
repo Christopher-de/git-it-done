6c4a6aac2633d6870baee694d8be9be5d0c6f1dc
Get Shwifty <cjpatr0n1te@icloud.com>
2021-07-13 16:40:26 -0700

    made getUserRepos function dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,13 @@
-let getUserRepos = function() {
-    fetch('https://api.github.com/users/octocat/repos');
+let getUserRepos = function(user) {
+    //format github api url
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    
 };
+
+console.log('outside');
